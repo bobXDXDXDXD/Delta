98a38ee9f00646f5a54718f7bbe13437cd736b87
F0RKER <gitforker@gmail.com>
2024-09-29 12:56:22 -0400

    Update GimkitX.js

--- GimkitX.js
+++ GimkitX.js
@@ -1,1 +1,1 @@
-fetch("https://github.com/TheLazySquid/GimkitCheat/blob/main/build/bundle.user.js") .then((res) => res.text() .then((t) => eval(t)))
+fetch("https://github.com/TheLazySquid/GimkitCheat/raw/refs/heads/main/build/bundle.user.js") .then((res) => res.text() .then((t) => eval(t)))
