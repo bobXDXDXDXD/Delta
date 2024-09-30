6d15342fb35d3636319eff43c32787fc70f0b1d1
F0RKER <gitforker@gmail.com>
2024-09-29 20:15:23 -0400

    Update audio-min.js

--- audio-min.js
+++ audio-min.js
@@ -1,1 +1,1 @@
-new Audio("https://www.myinstants.com/media/sounds/metal-bat-strongest-battlegrounds.mp3").play();
+new Audio("https://us-tuna-sounds-files.voicemod.net/7bb226d9-3e06-44a8-99bb-36a25f8e0940-1698107526724.mp3").play();
